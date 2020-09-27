4d3a949858999225813b8c1cf87cc0d6d1e8e571
bichnln <bich.nln@gmail.com>
2020-09-27 21:26:22 +1000

    add code for getting input from the form

--- app.py
+++ app.py
@@ -48,6 +48,10 @@ def process():
     if request.method == 'POST':
         f = request.files['cvfile']
 
+        # TODO: testing getting input
+        t = request.form['essential_skills']
+        print(t)
+
         #PDF Preview
         pdfstring = base64.b64encode(f.read())
         pdfstring = pdfstring.decode('ascii')
